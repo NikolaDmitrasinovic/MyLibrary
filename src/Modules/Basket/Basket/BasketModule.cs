148709b9a0286c066bc0a691b99181c25f207749
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-12-09 20:26:22 +0100

    feat: add UseBasketModule()

--- src/Modules/Basket/Basket/BasketModule.cs
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Basket;
@@ -14,5 +15,16 @@ public static class BasketModule
         //    .AddApiServices(configuration);
 
         return services;
+    }
+
+    public static IApplicationBuilder UseBasketModule(this IApplicationBuilder app)
+    {
+        // Configure the HTTP request pipeline.
+        //app
+        //    .UseApplicationServices()
+        //    .UseInfrastructureServices()
+        //    .UseApiServices();
+
+        return app;
     }
 }
