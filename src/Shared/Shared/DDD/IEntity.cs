b94d5e7cc367307f909ac47de870e6c7bc8de8f1
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-12-11 20:32:16 +0100

    feat: add IEntity

---
+++ src/Shared/Shared/DDD/IEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.DDD;
+public interface IEntity
+{
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
