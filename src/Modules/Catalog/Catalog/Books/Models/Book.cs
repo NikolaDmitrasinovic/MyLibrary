86d4d89129c0cee1ee64813d3ca36ff03098be15
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-12-16 21:12:32 +0100

    feat: add create method

--- src/Modules/Catalog/Catalog/Books/Models/Book.cs
+++ src/Modules/Catalog/Catalog/Books/Models/Book.cs
@@ -10,6 +10,22 @@ public class Book : Entity<Guid>
     public decimal Score { get; set; }
 
     // Create method fir initializin Book
+    public static Book Create(Guid id, string title, List<string> category, string description, string imageFile, decimal score)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(title);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(score);
+
+        var book = new Book
+        {
+            Id = id,
+            Title = title,
+            Category = category,
+            ImageFile = imageFile,
+            Score = score
+        };
+
+        return book;
+    }
     // Property setters private
     // Update method
 }
